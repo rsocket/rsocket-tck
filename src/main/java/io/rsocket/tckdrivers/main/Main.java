fb2f36a2b83880eb8e005740a17a04648c8bcb98
Yuri Schimke <yuri@schimke.ee>
2017-10-08 18:54:17 +0100

    fix help

--- src/main/java/io/rsocket/tckdrivers/main/Main.java
+++ src/main/java/io/rsocket/tckdrivers/main/Main.java
@@ -16,9 +16,7 @@
 
 package io.rsocket.tckdrivers.main;
 
-import io.airlift.airline.Command;
-import io.airlift.airline.Option;
-import io.airlift.airline.SingleCommand;
+import io.airlift.airline.*;
 import io.rsocket.Closeable;
 import io.rsocket.RSocket;
 import io.rsocket.RSocketFactory;
@@ -27,103 +25,121 @@ import io.rsocket.tckdrivers.common.TckClientTest;
 import io.rsocket.tckdrivers.server.JavaServerDriver;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import io.rsocket.transport.netty.server.TcpServerTransport;
+
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 import java.util.function.Predicate;
+
 import reactor.core.publisher.Mono;
 import reactor.ipc.netty.tcp.TcpServer;
 
-/** This class is used to run both the server and the client, depending on the options given */
+/**
+ * This class is used to run both the server and the client, depending on the options given
+ */
 @Command(
-  name = "rsocket-driver",
-  description = "This runs the client and servers that use the driver"
+        name = "rsocket-driver",
+        description = "This runs the client and servers that use the driver"
 )
-public class Main {
+public class Main extends HelpOption {
 
-  @Option(name = "--server", description = "set if you want to run the server")
-  public static boolean server;
+    @Option(name = "--server", description = "set if you want to run the server")
+    public static boolean server;
 
-  @Option(name = "--client", description = "set if you want to run the client")
-  public static boolean client;
+    @Option(name = "--client", description = "set if you want to run the client")
+    public static boolean client;
 
-  @Option(name = "--host", description = "The host to connect to for the client")
-  public static String host = "localhost";
+    @Option(name = "--host", description = "The host to connect to for the client")
+    public static String host = "localhost";
 
-  @Option(name = "--port", description = "The port")
-  public static int port = 30006;
+    @Option(name = "--port", description = "The port")
+    public static int port = 30006;
 
-  @Option(
-    name = "--file",
-    description =
-        "The script file to parse, make sure to give the client and server the " + "correct files"
-  )
-  public static File file;
+    @Option(
+            name = "--file",
+            description =
+                    "The script file to parse, make sure to give the client and server the " + "correct files"
+    )
+    public static File file;
 
-  @Option(
-    name = "--tests",
-    description =
-        "For the client only, optional argument to list out the tests you"
-            + " want to run, should be comma separated names"
-  )
-  public static List<String> tests;
+    @Option(
+            name = "--tests",
+            description =
+                    "For the client only, optional argument to list out the tests you"
+                            + " want to run, should be comma separated names"
+    )
+    public static List<String> tests;
 
-  /**
-   * A function that parses the test file, and run each test
-   *
-   * @param host client connects with this host
-   * @param port client connects on this port
-   * @param testFilter predicate for tests to run
-   */
-  public static void runTests(String host, int port, Predicate<String> testFilter) {
-    TcpClientTransport transport = TcpClientTransport.create(host, port);
-    Mono<RSocket> clientBuilder = RSocketFactory.connect().transport(transport).start();
+    /**
+     * A function that parses the test file, and run each test
+     *
+     * @param host       client connects with this host
+     * @param port       client connects on this port
+     * @param testFilter predicate for tests to run
+     */
+    public static void runTests(String host, int port, Predicate<String> testFilter) {
+        TcpClientTransport transport = TcpClientTransport.create(host, port);
+        Mono<RSocket> clientBuilder = RSocketFactory.connect().transport(transport).start();
 
-    System.out.println("test file: " + file);
+        System.out.println("test file: " + file);
 
-    for (TckClientTest t : TckClientTest.extractTests(file)) {
-      if (testFilter.test(t.name)) {
-        System.out.println("Running " + t.name);
-        JavaClientDriver jd = new JavaClientDriver(clientBuilder);
-        jd.runTest(t);
-      }
-    }
-  }
-
-  public static void main(String[] args) throws IOException {
-    SingleCommand<Main> cmd = SingleCommand.singleCommand(Main.class);
-    cmd.parse(args);
-
-    if (server) {
-      if (file == null) {
-        file = new File("rsocket-tck-drivers/src/test/resources/servertest.txt");
-      }
-      runServer();
-    } else if (client) {
-      if (file == null) {
-        file = new File("rsocket-tck-drivers/src/test/resources/clienttest.txt");
-      }
-      try {
-        if (tests != null) {
-          runTests(host, port, name -> tests.contains(name));
-        } else {
-          runTests(host, port, t -> true);
+        for (TckClientTest t : TckClientTest.extractTests(file)) {
+            if (testFilter.test(t.name)) {
+                System.out.println("Running " + t.name);
+                JavaClientDriver jd = new JavaClientDriver(clientBuilder);
+                jd.runTest(t);
+            }
         }
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
     }
-  }
 
-  private static void runServer() throws IOException {
-    TcpServerTransport server = TcpServerTransport.create(TcpServer.create(port));
+    public static void main(String[] args) throws IOException {
+        SingleCommand<Main> cmd = SingleCommand.singleCommand(Main.class);
+        try {
+            Main main = cmd.parse(args);
 
-    JavaServerDriver jsd = JavaServerDriver.read(file);
+            main.run();
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            Help.help(cmd.getCommandMetadata());
+            System.exit(-1);
+        }
+    }
 
-    Closeable socket =
-        RSocketFactory.receive().acceptor(jsd.acceptor()).transport(server).start().block();
+    private void run() throws IOException {
+        if (showHelpIfRequested()) {
+            System.exit(0);
+        }
 
-    socket.onClose().block(Duration.ofDays(365));
-  }
+        if (server) {
+            if (file == null) {
+                file = new File("rsocket-tck-drivers/src/test/resources/servertest.txt");
+            }
+            runServer();
+        } else if (client) {
+            if (file == null) {
+                file = new File("rsocket-tck-drivers/src/test/resources/clienttest.txt");
+            }
+            try {
+                if (tests != null) {
+                    runTests(host, port, name -> tests.contains(name));
+                } else {
+                    runTests(host, port, t -> true);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void runServer() throws IOException {
+        TcpServerTransport server = TcpServerTransport.create(TcpServer.create(port));
+
+        JavaServerDriver jsd = JavaServerDriver.read(file);
+
+        Closeable socket =
+                RSocketFactory.receive().acceptor(jsd.acceptor()).transport(server).start().block();
+
+        socket.onClose().block(Duration.ofDays(365));
+    }
 }
